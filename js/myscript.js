8434c97eeb2c21ea3cd414ad5d554271de804e9e
riskiputraalamzah <aryainggil3@gmail.com>
2021-11-08 22:00:08 +0700

    menambahkan konten untuk beberapa menu

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,7 @@
-const whatsApp = document.querySelector("button.whatsapp");
+const whatsApp = document.querySelectorAll("button.whatsapp");
 const number = "6282152519079";
-whatsApp.addEventListener("click", function () {
-  window.open(`https://api.whatsapp.com/send?phone=${number}`, "_blank");
+whatsApp.forEach((e) => {
+  e.addEventListener("click", function () {
+    window.open(`https://api.whatsapp.com/send?phone=${number}`, "_blank");
+  });
 });
